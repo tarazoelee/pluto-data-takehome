6ee4d4694cadf2bb659fbe5dd29768a67a128b9b
Tara Lee <tlee379@uwo.ca>
2025-01-28 22:35:05 +0000

    feat: away team select depends on home team

--- frontend/src/Components/GameSelection.jsx
+++ frontend/src/Components/GameSelection.jsx
@@ -15,7 +15,9 @@ const useStyles = makeStyles({
 
 function GameSelection() {
   const [homeTeams, setHomeTeams] = useState([]);
-  const [selectedTeam, setSelectedTeam] = useState("");
+  const [awayTeams, setAwayTeams] = useState([]);
+  const [selectedHomeTeam, setHomeTeam] = useState("");
+  const [selectedAwayTeam, setAwayTeam] = useState("");
   const classes = useStyles();
 
   const fetchHomeTeams = async () => {
@@ -27,36 +29,80 @@ function GameSelection() {
     }
   };
 
+  const fetchAwayTeams = async () => {
+    try {
+      const res = await api.get(`/get_away_teams/${selectedHomeTeam}`);
+      setAwayTeams(res.data);
+    } catch (err) {
+      console.error("Error getting teams");
+    }
+  };
+
   useEffect(() => {
     fetchHomeTeams();
   }, []);
 
-  const handleChange = (event) => {
-    setSelectedTeam(event.target.value);
+  // Only fetch away teams if a home team is selected
+  useEffect(() => {
+    if (selectedHomeTeam) {
+      fetchAwayTeams();
+    }
+  }, [selectedHomeTeam]);
+
+  const handleSelectHomeTeam = (event) => {
+    setHomeTeam(event.target.value);
+  };
+
+  const handleSelectAwayTeam = (event) => {
+    setAwayTeam(event.target.value);
   };
 
   return (
     <>
       <Grid container spacing={2}>
-        <FormControl className={classes.selectContainer} variant="filled">
-          <InputLabel className={classes.inputLabel}>Home Team</InputLabel>
-          <Select
-            value={selectedTeam}
-            onChange={handleChange}
-            label="Select Home Team"
-            className={classes.select}
-          >
-            {homeTeams.length > 0 ? (
-              homeTeams.map((team, index) => (
-                <MenuItem key={index} value={team}>
-                  {team}
-                </MenuItem>
-              ))
-            ) : (
-              <MenuItem disabled>No Teams Available</MenuItem>
-            )}
-          </Select>
-        </FormControl>
+        <Grid item>
+          <FormControl fullWidth className={classes.formControl}>
+            <InputLabel className={classes.inputLabel}>Home Team</InputLabel>
+            <Select
+              value={selectedHomeTeam}
+              onChange={handleSelectHomeTeam}
+              label="Select Home Team 1"
+              className={classes.select}
+            >
+              {homeTeams.length > 0 ? (
+                homeTeams.map((team, index) => (
+                  <MenuItem key={index} value={team}>
+                    {team}
+                  </MenuItem>
+                ))
+              ) : (
+                <MenuItem disabled>No Teams Available</MenuItem>
+              )}
+            </Select>
+          </FormControl>
+        </Grid>
+
+        <Grid item>
+          <FormControl fullWidth className={classes.formControl}>
+            <InputLabel className={classes.inputLabel}>Away Team</InputLabel>
+            <Select
+              value={selectedAwayTeam}
+              onChange={handleSelectAwayTeam}
+              label="select away team"
+              className={classes.select}
+            >
+              {awayTeams.length > 0 ? (
+                awayTeams.map((team, index) => (
+                  <MenuItem key={index} value={team}>
+                    {team}
+                  </MenuItem>
+                ))
+              ) : (
+                <MenuItem disabled>No Teams Available</MenuItem>
+              )}
+            </Select>
+          </FormControl>
+        </Grid>
       </Grid>
     </>
   );
