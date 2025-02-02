78c44499d1400714ca3f1488201af2d3e87e7001
Tara Lee <tlee379@uwo.ca>
2025-02-02 17:59:26 +0000

    fix: styling game info

--- frontend/src/Components/GameInfo.jsx
+++ frontend/src/Components/GameInfo.jsx
@@ -10,6 +10,13 @@ const useStyles = makeStyles({
     fontWeight: 900,
     textAlign: "center",
   },
+  gameInfo: {
+    margin: "10px 0px 10px 0px",
+    fontSize: "14px",
+    color: "white",
+    fontWeight: 700,
+    textAlign: "center",
+  },
 });
 
 function GameInfo(props) {
@@ -35,12 +42,12 @@ function GameInfo(props) {
   }, [props]);
 
   return (
-    <Grid item container direction="column" className={classes.winPercent}>
-      <Grid>
+    <Grid item container direction="column">
+      <Grid className={classes.gameInfo}>
         {props.home_team} vs. {props.away_team}
         {props.venue && ` at ${props.venue}`}
       </Grid>
-      <Grid>
+      <Grid className={classes.winPercent}>
         {props.home_team} win {winPercentage}% of the time!
       </Grid>
     </Grid>
